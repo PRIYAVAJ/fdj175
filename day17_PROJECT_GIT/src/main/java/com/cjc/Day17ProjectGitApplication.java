58f3829b00a35559eb4c3cf5ac8d5c0db3a8bf13
PRIYAVAJ <priyankavaj1006@gmail.com>
2024-05-31 14:52:02 +0530

    done commit

---
+++ day17_PROJECT_GIT/src/main/java/com/cjc/Day17ProjectGitApplication.java
@@ -0,0 +1,13 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day17ProjectGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day17ProjectGitApplication.class, args);
+	}
+
+}
