d3ba08e1e1a25388dcc099ae188595f0493f526a
PRIYAVAJ <priyankavaj1006@gmail.com>
2024-05-31 16:04:30 +0530

    done

--- day17_PROJECT_GIT/src/main/java/com/cjc/controller/HomeController.java
+++ day17_PROJECT_GIT/src/main/java/com/cjc/controller/HomeController.java
@@ -1,15 +1,34 @@
 package com.cjc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cjc.model.Student;
+import com.cjc.serviceI.StudentServiceI;
 
 @RestController
 public class HomeController 
 {
+	@Autowired
+	StudentServiceI ssi;
 	@PostMapping("/addStu")
 	public String addStudent()
 	{
 		return "added successfully";
+	}
+	@DeleteMapping("/deletedata/{rollno}")
+	public String deletdata(@RequestBody Student s)
+	{
+		ssi.save(s);
+		
+		return "data deleted";
+		
 	}
 
 }
