d81b5470b160ad0c31792fb96709969bae3f6a80
PRIYAVAJ <priyankavaj1006@gmail.com>
2024-05-31 16:02:04 +0530

    done new commit

--- day17_PROJECT_GIT/src/main/java/com/cjc/controller/HomeController.java
+++ day17_PROJECT_GIT/src/main/java/com/cjc/controller/HomeController.java
@@ -1,4 +1,8 @@
 package com.cjc.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HomeController 
 {
--- day17_PROJECT_GIT/src/main/java/com/cjc/serviceImpl/StudentServiceImpl.java
+++ day17_PROJECT_GIT/src/main/java/com/cjc/serviceImpl/StudentServiceImpl.java
@@ -1,5 +1,18 @@
 package com.cjc.serviceImpl;
 
-public class StudentServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cjc.model.Student;
+import com.cjc.repository.StudentRepository;
+import com.cjc.serviceI.StudentServiceI;
+
+public class StudentServiceImpl implements StudentServiceI {
+
+	@Autowired
+	StudentRepository sr;
+	@Override
+	public void save(Student s) {
+		
+	}
 
 }
